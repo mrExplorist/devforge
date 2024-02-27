12d1e3377d4bb9fa3b9009cd860540aac79e0863
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-27 19:13:15 +0530

    Add(Utils): Add the cx utility function to the project, enabling efficient concatenation of CSS classes within components. This function enhances readability and maintainability by abstracting away string concatenation or template literals in JSX, ensuring cleaner and more concise code.

---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+// TODO: Add more utility functions here
+
+export const cx = (...args) => args.filter(Boolean).join(' '); // this is helper function to join classes together in a component to avoid using string concatenation or template literals in the JSX

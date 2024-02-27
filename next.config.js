72285097e6e913a1f8f69bcb155bf27da7c0899b
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-27 20:10:25 +0530

    feat(Contentlayer SDK plugin) 🔌 : Configured Next.js app with Contentlayer plugin for validating and tranforming content(Markdown) into type-safe JSON data 🚀.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+// Next Contentlayer Setup
+
+const { withContentlayer } = require('next-contentlayer');
+
+const nextConfig = {};
+
+module.exports = withContentlayer({ ...nextConfig });

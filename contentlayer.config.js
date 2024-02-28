05559fe90995d847f868f238e3d76ba28e35a50d
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-28 22:48:02 +0530

    feat(contentlayer documentType) 🏷 : Define Blog document type and create content source.

---
+++ contentlayer.config.js
@@ -0,0 +1,60 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: `**/**/*.mdx`,
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the post',
+      required: true,
+    },
+
+    publishedAt: {
+      type: 'date',
+      description: 'The date of the post',
+      required: true,
+    },
+    updatedAt: {
+      type: 'date',
+      description: 'The date of the updation of the post',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      description: 'The description of the post',
+      required: true,
+    },
+    image: {
+      type: 'image',
+      description: 'The image of the post',
+      required: true,
+    },
+    isPublished: {
+      type: 'boolean',
+      default: true,
+    },
+    author: {
+      type: 'string',
+      description: 'The author of the post',
+      required: true,
+    },
+    tags: {
+      type: 'list',
+      of: {
+        type: 'string',
+      },
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Blog],
+});

114fb6046151e5c483150e43e2f696bfd6e3c8c3
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-01 08:54:03 +0530

    feat(utility function): Implement cn function to merge Tailwind CSS classes efficiently using twMerge from 'tailwind-merge' library.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
 // TODO: Add more utility functions here
 
+import { compareDesc, parseISO } from 'date-fns';
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// Function to merge Tailwind CSS classes together
+// Create a new file named cn.js or any preferred name
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
 export const cx = (...args) => args.filter(Boolean).join(' '); // this is helper function to join classes together in a component to avoid using string concatenation or template literals in the JSX

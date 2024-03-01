7031d85761baa9aea1e4bd02e66bae6cd5c2f4f3
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-01 19:41:12 +0530

    Feat(Tailwind Config): ⚒ Configured Tailwind CSS with custom theme extensions, including additional colors, background images, and font families. Defined content paths for Tailwind's PurgeCSS to optimize bundle size by removing unused CSS from specified files. Added Tailwind scrollbar plugin for custom scrollbar styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,18 @@ module.exports = {
         accentDark: '#ffdb4d',
         gray: '#747474',
       },
+      backgroundImage: {
+        'dot-white': 'radial-gradient(white 1px, transparent 1px)',
+        'dot-black': 'radial-gradient(black 1px, transparent 1px)',
+      },
     },
     fontFamily: {
       mr: ['var(--font-mr)'],
       in: ['var(--font-inter)'],
+      //   os: ['var(--font-os)'],
+      //   inc: ['var(--font-inc)'],
+      scp: ['var(--font-scp)'],
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 };

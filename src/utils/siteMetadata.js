5c0dc9a4aa9332a935b4e31897f279178f8d34b2
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-01 19:47:13 +0530

    Feat(SEO Metadata): 🚀 Configured SEO metadata including site title, author, header title, description, language, theme preferences, site URL, logos, social media links, and locale for optimal search engine visibility.

---
+++ src/utils/siteMetadata.js
@@ -0,0 +1,21 @@
+const siteMetadata = {
+  title: 'BitStack',
+  author: 'Lalit Sharma',
+  headerTitle: 'BitStack | Web Development, Programming, and Technology Blog.',
+  description: '5 Minute Read on latest trends in Web Development, Programming, and Technology made with Contentlayer.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://bitstack.vercel.app', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'mailfor.lalitsharma@gmail.com',
+  github: 'https://github.com/mrExplorist',
+  twitter: 'https://twitter.com/Mr_Explorist',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com/',
+  linkedin: 'https://www.linkedin.com/in/pinglalit/',
+  dribbble: 'https://www.dribbble.com',
+  locale: 'in-IN',
+};
+
+module.exports = siteMetadata;

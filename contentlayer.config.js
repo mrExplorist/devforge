982f1eed509f6c5e317e7fd93f51d95d22dcffd9
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-05 17:36:21 +0530

    Feat(contentlayer config for Table Of Contents) 🚀: Add a computed field toc to generate a table of contents based on document headings, utilizing regular expressions and GithubSlugger for slug generation. The field returns an array of objects with heading level, text content, and slugs for creating the table of contents.

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,5 +1,6 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 
+import GithubSlugger from 'github-slugger';
 import readingTime from 'reading-time';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypePrettyCode from 'rehype-pretty-code';
@@ -61,6 +62,25 @@ const Blog = defineDocumentType(() => ({
     readingTime: {
       type: 'json',
       resolve: (doc) => readingTime(doc.body.raw),
+    },
+    toc: {
+      type: 'json',
+      resolve: async (doc) => {
+        const regulrExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const slugger = new GithubSlugger();
+        const headings = Array.from(doc.body.raw.matchAll(regulrExp)).map(({ groups }) => {
+          const flag = groups?.flag;
+          const content = groups?.content;
+
+          return {
+            level: flag?.length == 1 ? 'one' : flag?.length == 2 ? 'two' : 'three',
+            text: content,
+            slug: content ? slugger.slug(content) : undefined,
+          };
+        });
+
+        return headings;
+      },
     },
   },
 }));

6c4751965cc4053d01e2241c7bd8feb7838d3919
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-07 09:38:52 +0530

    Feat(Progressbar): Implemented Progressbar component to display reading progress on scroll for target element.

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+export const Progressbar = ({ target }) => {
+  const [readingProgress, setReadingProgress] = useState(0);
+
+  const scrollListener = useCallback(() => {
+    if (!target.current) {
+      return;
+    }
+
+    const element = target.current;
+    const totalHeight = element.clientHeight - element.offsetTop - window.innerHeight;
+    const windowScrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (windowScrollTop === 0) {
+      return setReadingProgress(0);
+    }
+
+    if (windowScrollTop > totalHeight) {
+      return setReadingProgress(100);
+    }
+
+    setReadingProgress((windowScrollTop / totalHeight) * 100);
+  }, [target]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollListener);
+
+    return () => window.removeEventListener('scroll', scrollListener);
+  }, [scrollListener]);
+
+  return (
+    <div className="w-full fixed top-0 left-0 right-0">
+      <div
+        className="h-2 bg-gradient-to-r from-[#FB7C00] via-[#E73B50] to-[#9E009B]"
+        style={{
+          width: `${readingProgress}%`,
+        }}
+      />
+    </div>
+  );
+};

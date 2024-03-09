2908376f1145871bc6f281b4acdb75bb70c436ad
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-09 11:08:31 +0530

    Feat(Lottie player lottie animation): Added LottieAnimation component to render a Lottie animation using the DotLottiePlayer component from '@dotlottie/react-player'. Applied autoplay and loop attributes to enable automatic playback and looping of the animation.

---
+++ src/components/Contact/LottieAnimation.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import '@dotlottie/react-player/dist/index.css';
+
+import { DotLottiePlayer } from '@dotlottie/react-player';
+import React from 'react';
+
+const LottieAnimation = () => {
+  return <DotLottiePlayer src="/animation_llqd7ey4.lottie" autoplay loop></DotLottiePlayer>;
+};
+
+export default LottieAnimation;

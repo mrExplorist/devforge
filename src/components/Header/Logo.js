a9db8650548a3adceb5ed7ac729d8c71c5a20622
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-09 10:49:05 +0530

    Refactor Logo component for improved structure and responsiveness.

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import BlogLogo from '@/public/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex justify-center items-center text-dark">
-      <div className="overflow-hidden border border-solid border-dark">
-        <Image src={BlogLogo} alt="BitStack" width={80} height={80} />
+    <Link href="/" className="flex justify-center items-center ">
+      <div className="rounded-lg px-2 text-xl font-bold transition-all hover:-translate-y-[2px]  dark:border-gray-800 text-color-light flex items-center justify-center ">
+        <div className="overflow-hidden items-center justify-center w-14 h-14 md:w-[80px] md:h-[80px]">
+          <Image src={BlogLogo} alt="BitStack" />
+        </div>
+        <span className="font-in text-3xl hidden md:block capitalize">
+          bitstack<span className="gradient-text animate-gradient">.</span>
+        </span>
       </div>
-      <span className="text-2xl font-bold text-cyan-200">BitStack</span>
     </Link>
   );
 };

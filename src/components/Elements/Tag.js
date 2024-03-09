bbd447c8622096ca5af9c29937dc48bd73c4e53c
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-09 10:52:18 +0530

    Refactor the Tag component to use next/link for improved accessibility and SEO by replacing the <a> tag with the Link component from Next.js.

--- src/components/Elements/Tag.js
+++ src/components/Elements/Tag.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import Link from 'next/link';
+import React from 'react';
 import { cx } from '@/src/utils';
 
 const Tag = ({ link = '#', name, ...props }) => {
@@ -7,7 +7,7 @@ const Tag = ({ link = '#', name, ...props }) => {
     <Link
       href={link}
       className={cx(
-        'inline-block py-3 px-10 bg-[#1d1c20] text-light rounded-full capitalize font-semibold  duration-200 ease-in-out hover:border-indigo-800  hover:shadow-md transition-all cursor-pointer ',
+        'inline-block py-3 px-6 bg-dark-charcoal text-light rounded-full capitalize font-semibold  duration-200 ease-in-out border-indigo-800 hover:shadow-lg transition-all cursor-pointer relative text-sm ',
         props.className,
       )}
     >

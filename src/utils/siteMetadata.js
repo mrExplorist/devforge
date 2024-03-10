f392e995bad591453c0073a320272bf080bb8cbc
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-10 11:56:47 +0530

    Fix(Metadata fixes)

--- src/utils/siteMetadata.js
+++ src/utils/siteMetadata.js
@@ -1,20 +1,21 @@
 const siteMetadata = {
-  title: 'BitStack ',
+  title: 'devforge — Resources for devs',
   author: 'Lalit Sharma',
-  headerTitle: 'BitStack | Web Development, Programming, and Technology Blog.',
+  headerTitle: 'Devforge | Web Development, Programming, and Technology Blog.',
   description: '5 Minute Read on latest trends in Web Development, Programming, and Technology made with Contentlayer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://bitstack.vercel.app', // your website URL
+  siteUrl: 'https://devforge.vercel.app', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png',
   email: 'mailfor.lalitsharma@gmail.com',
   github: 'https://github.com/mrExplorist',
   twitter: 'https://twitter.com/Mr_Explorist',
-  facebook: 'https://facebook.com',
+  discord: 'https://discordapp.com/',
+  telegram: 'https://telegram.org/',
+  instagram: 'https://instagram.com',
   youtube: 'https://youtube.com/',
   linkedin: 'https://www.linkedin.com/in/pinglalit/',
-  dribbble: 'https://www.dribbble.com',
   locale: 'en-US',
 };
 

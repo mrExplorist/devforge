d2939a4f4c3c05cc398c99b178815247b19d5be8
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-10 12:02:17 +0530

    Fixes(RenderMDX markdown render): Implemented the RenderMdx component for rendering MDX content dynamically. The component utilizes the useMDXComponent hook from next-contentlayer/hooks to render the MDX content fetched from the blog body. It uses a predefined set of MDX components, including Image, to handle different types of content elements. The rendered content is displayed within a prose container with custom styling for blockquotes and list markers, ensuring consistent typography and design across the blog posts.

--- src/components/Blog/RenderMdx.jsx
+++ src/components/Blog/RenderMdx.jsx
@@ -11,7 +11,7 @@ const mdxComponents = {
 const RenderMdx = ({ blog }) => {
   const MDXContent = useMDXComponent(blog.body.code);
   return (
-    <div className="col-span-12 lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max prose-blockquote:bg-color-accent/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-color-accent prose-blockquote:not-italic prose-blockquote:rounded-r-lg prose-li:marker:text-blue-500 dark:prose-invert dark:prose-blockquote:border-color-accentDark dark:prose-blockquote:bg-color-accentDark/20 dark:prose-li:marker:text-accentDark first-letter:text-3xl sm:first-letter:text-5xl">
+    <div className="col-span-12 lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max prose-blockquote:bg-color-accent/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-color-accent prose-blockquote:not-italic prose-blockquote:rounded-r-lg prose-li:marker:text-light-gray dark:prose-invert dark:prose-blockquote:border-color-accentDark dark:prose-blockquote:bg-color-accentDark/20 dark:prose-li:marker:text-accentDark first-letter:text-3xl sm:first-letter:text-5xl">
       <MDXContent components={mdxComponents} />
     </div>
   );
